1d0c5366dd392aab5d0b8061abc93cfa14bc47e2
hamza202 <k8668975@gmail.com>
2019-10-16 04:31:04 +0300

    home page finsh

--- js/main.js
+++ js/main.js
@@ -5,3 +5,40 @@ $(function() {
         scrollspeed: 100
     });
 });
+
+var $document = $(document)
+$document.scroll(function () {
+        if ($document.scrollTop() >= 50) {
+            $('.counter').each(function () {
+                var $this = $(this),
+                    countTo = $this.attr('data-count');
+
+                $({countNum: $this.text()}).animate({
+                        countNum: countTo
+                    },
+                    {
+                        duration: 2000,
+                        easing: 'linear',
+                        step: function () {
+                            $this.text(Math.floor(this.countNum));
+                        },
+                        complete: function () {
+                            $this.text(this.countNum);
+                            //alert('finished');
+                        }
+                    });
+            });
+        }
+    }
+);
+
+
+
+$(document).ready(function()
+{
+    $(".loading-overly").fadeOut(1500,
+        function()
+        {
+            $(this).remove();
+        });
+});
