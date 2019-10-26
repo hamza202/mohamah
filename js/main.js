35e0476d41f90354c8e8d26b1fcd01f6fe11907b
hamza202 <k8668975@gmail.com>
2019-10-26 13:35:20 +0200

    form

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-
 function detectmob() {
-    if(window.innerWidth >= 993 ) {
-        $(function() {
+    if (window.innerWidth >= 993) {
+        $(function () {
             $("body").niceScroll({
                 cursorborder: "0",
-                cursorcolor:"#D59755",
+                cursorcolor: "#D59755",
                 scrollspeed: 100
             });
         });
@@ -12,19 +11,98 @@ function detectmob() {
         console.log('false')
     }
 }
+
 detectmob();
-$(document).ready(function()
-{
+$(document).ready(function () {
     $(".loading-overly").fadeOut(1500,
-        function()
-        {
+        function () {
             $(this).remove();
         });
 });
-$(window).scroll(function(){
+$(window).scroll(function () {
     var sticky = $('.sticky'),
         scroll = $(window).scrollTop();
 
     if (scroll >= 200) sticky.addClass('fixed');
     else sticky.removeClass('fixed');
+});
+
+//wizard
+$(function () {
+    var $signupForm = $('#SignupForm');
+
+    $signupForm.validate({
+        rules: {
+            gender: {
+                required: true,
+            },
+            country: {
+                required: true,
+            },
+            nationality: {
+                required: true,
+            },
+            re_password: {
+                equalTo: "#Password"
+            },
+            date: {
+                dateBR: {dateBR: true}
+            }
+
+        },
+        messages: {
+            country: "الرجاء اختر دولتك",
+            nationality: "الرجاء اختر جنسيتك",
+            phone: {
+                maxlength: $.validator.format(" الرقم غير صحيح"),
+                minlength: $.validator.format("الرقم غير صحيح"),
+            }
+        }
+    });
+
+    $signupForm.formToWizard({
+        submitButton: 'SaveAccount',
+        nextBtnClass: 'my-btn btn-4 next',
+        nextBtnName: 'التالي',
+        prevBtnName: 'السابق',
+        prevBtnClass: 'btn btn-secondary btn-4 prev',
+        buttonTag: 'button',
+        validateBeforeNext: function (form, step) {
+            var stepIsValid = true;
+            var validator = form.validate();
+            $(':input', step).each(function (index) {
+                var xy = validator.element(this);
+                stepIsValid = stepIsValid && (typeof xy == 'undefined' || xy);
+            });
+            return stepIsValid;
+        },
+        progress: function (i, count) {
+            $('#progress-complete').width('' + (i / count * 100) + '%');
+        }
+    });
+});
+
+
+$('.disabled-click').on('click', function () {
+    $('#acc1').attr("disabled", true);
+    Swal.fire({
+        type: 'error',
+        title: 'عذرا',
+        confirmButtonText: 'اغلاق',
+        confirmButtonColor: '#6c757d',
+        text: 'التسجيل في الوقت الحالي متاح للمحامين فقط',
+    })
+});
+
+var picker = new Pikaday({
+    yearRange: [1930, 2019], field: document.getElementById('datepicker'), format: 'dd/mm/yy',
+    toString(date, format) {
+        // you should do formatting based on the passed format,
+        // but we will just return 'D/M/YYYY' for simplicity
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+        console.log(year);
+        return `${day}/${month}/${year}`;
+    },
 });
